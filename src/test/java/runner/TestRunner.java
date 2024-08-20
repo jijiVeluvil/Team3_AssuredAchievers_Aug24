ffbcaf7f1b6498597cfd4828b3a0b655a9b51b07
nehasaini09 <116294426+nehasaini09@users.noreply.github.com>
2024-08-20 09:27:37 -0700

    Allure report

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -4,10 +4,11 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		plugin= {"pretty","html:target/Team3_AssuredAchievers.html",
-				//"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-				//"pretty","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
-		},
+		plugin = {"pretty", "html:target/dietician.html",
+				"json:target/dietician.json",
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+				"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+				}, 
 		monochrome=false,
 		dryRun=false,
 		features= {"src/test/resources/features/01userLoginModule1.feature",
